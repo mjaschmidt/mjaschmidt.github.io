4d7b68d4c43fe49bb67728104571c3ead4920e23
mjaschmidt <48790655+mjaschmidt@users.noreply.github.com>
2019-03-24 00:54:19 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 
 function youSure(value){
 
-    if(window.confirm("Supporting me gives your song much better odds and you only pay if you find placement (5$). Continue submitting without support?")){
+    if(window.confirm("Supporting me gives your song much better odds and you only pay if you can get placement (5$). At which point you can still decide to not pay. Continue submitting without support?")){
 
     }
     else {
