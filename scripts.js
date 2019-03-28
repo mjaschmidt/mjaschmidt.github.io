256c41084f3bdee8b4bb3a8c2a8d65202aa79f76
mjaschmidt <48790655+mjaschmidt@users.noreply.github.com>
2019-03-28 16:18:21 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,14 +1,41 @@
+function youSure(value) {
 
-function youSure(value){
+    if (window.confirm("Supporting me gives your song better odds and you only pay if you can get actual placement. Continue submitting without support?")) {
 
-    if(window.confirm("Supporting me gives your song much better odds and you only pay if you can get placement (5$). At which point you can still decide to not pay. Continue submitting without support?")){
-
-    }
-    else {
-    document.getElementById("inputEmail").value = "";
+    } else {
+        document.getElementById("inputEmail").value = "";
     }
 }
 
-function updateSubject(value){
+function updateSubject(value) {
     document.getElementById("emailSubject").value = document.getElementById("inputEmail").value;
-} 
+}
+
+let emoteArray = ['🤗', 'ah thats hot!', '💪', 'dope!', 'you rock!', '🔥', 'yuh', '💦', '🤑', 'click!', '😇', '🙏', '😍', '🤤', 'yay', 'you the one', '😩', '🎵', '🎶', '🎧', '💸', '✨', '🍆', '🍑', '💨', '💞', '💓', '💕', '💙', '💜', '💚', '💛', '🧡', '🆒', '🙆', '🙌'];
+let randomInt = 0;
+
+// setInterval(function(){
+//     randNum();
+//     document.getElementById('heart').innerHTML = emoteArray[randomInt];
+// }, 1000)
+
+// function randNum() {
+//     randomInt = Math.floor(Math.random() * 36);
+// }
+
+function beatHearts() {
+    document.getElementById("beatinghearts").style.visibility = "visible";
+    // randNum();
+    // document.getElementById('heart').innerHTML = emoteArray[randomInt];
+}
+
+function stopHearts() {
+    document.getElementById("beatinghearts").style.visibility = "hidden";
+}
+
+// function breakHeart(){
+//     document.getElementById("ogHeart").innerHTML = "💔";
+// }
+// function repairHeart(){
+//     document.getElementById("ogHeart").innerHTML = "💕";
+// }
