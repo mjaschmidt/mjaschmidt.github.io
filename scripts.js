4e8894f76cf96236838a9abbd9ff477351a66048
mjaschmidt <48790655+mjaschmidt@users.noreply.github.com>
2019-04-06 15:11:36 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-function youSure(value) {
+// function youSure(value) {
 
-    if (window.confirm("Supporting me gives your song better odds and you only pay if you can get actual placement. Continue submitting without support?")) {
+//     if (window.confirm("Supporting me gives your song better odds and you only pay if you can get actual placement. Continue submitting without support?")) {
 
-    } else {
-        document.getElementById("inputEmail").value = "";
-    }
-}
+//     } else {
+//         document.getElementById("inputEmail").value = "";
+//     }
+// }
 
 function updateSubject(value) {
     document.getElementById("emailSubject").value = document.getElementById("inputEmail").value;
